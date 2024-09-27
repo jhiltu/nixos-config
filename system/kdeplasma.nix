55e8aadc2713e058cae86177d3663020cc08941a
Joni Hiltunen <joni@jonihiltunen.com>
2024-09-28 00:31:25 +0300

    Disable NetworkManager-wait-online

--- system/kdeplasma.nix
+++ system/kdeplasma.nix
@@ -5,7 +5,17 @@
   services.displayManager.sddm.wayland.enable = true;
   services.desktopManager.plasma6.enable = true;
 
+  # Mail, organizer, etc
+  /*programs.kde-pim = {
+    enable = true;
+    kmail = true;
+    kontact = true;
+  };*/
+
   environment.systemPackages = with pkgs; [
     kate
+
+    # Kontact suite addon
+    #korganizer
   ];
 }
