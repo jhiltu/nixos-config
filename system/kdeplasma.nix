c6c5db9b7daad2cb6978bc6ce24a316a14111d95
Joni Hiltunen <joni@jonihiltunen.com>
2024-09-28 00:33:34 +0300

    Revert "Disable NetworkManager-wait-online"
    This reverts commit 55e8aadc2713e058cae86177d3663020cc08941a.

--- system/kdeplasma.nix
+++ system/kdeplasma.nix
@@ -5,17 +5,7 @@
   services.displayManager.sddm.wayland.enable = true;
   services.desktopManager.plasma6.enable = true;
 
-  # Mail, organizer, etc
-  /*programs.kde-pim = {
-    enable = true;
-    kmail = true;
-    kontact = true;
-  };*/
-
   environment.systemPackages = with pkgs; [
     kate
-
-    # Kontact suite addon
-    #korganizer
   ];
 }
