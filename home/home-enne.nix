2ae5f45d1fd62317cae6ccd0aa2f96c993137e56
Joni Hiltunen <joni@jonihiltunen.com>
2024-09-28 19:29:58 +0300

    Manage Doom Emacs config

--- home/home-enne.nix
+++ home/home-enne.nix
@@ -1,7 +1,12 @@
-{ pkgs, ... }:
+{ pkgs, config, ... }:
 {
   imports = [
     ./home-common.nix
   ];
+
+  home.file = {
+    ".config/doom".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/nixos-config/dotfiles/.config/doom";
+  };
+
   home.stateVersion = "24.05"; # Don't touch!
 }
